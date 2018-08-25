be963ca70d9c2c14347a2e486fe8e9193cb4b4f5
BeeCoding <rafael@tunning.com.br>
2018-08-25 13:00:12 -0300

    [STANDARDJS] Better code style

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const config = require('./config.json')
 mongoose.connect(config.db, { useNewUrlParser: true })
 
 const Url = mongoose.model('Url', {
-    url: String,
-    shorturl: String,
-    iscustom: Boolean,
-    clicks: { type: Number, default: 0 },
-    date: { type: Date, default: Date.now }
+  url: String,
+  shorturl: String,
+  iscustom: Boolean,
+  clicks: { type: Number, default: 0 },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 app.set('view engine', 'ejs')
@@ -19,50 +22,50 @@ app.use(express.static('static'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index', config)
+  res.render('index', config)
 })
 
 app.post('/short', async (req, res) => {
-    var shorturl = false
-    var iscustom = false
-    if (!req.body.url) return res.json({sucess: false, err: "No url provided"})
-    if (!/http:\/\/|https:\/\//g.test(req.body.url)) return res.json({sucess: false, err: "Invalid url"})
-    if (req.body.custom) {
-        if (/[^a-zA-Z0-9]/g.test(req.body.custom)) return res.json({sucess: false, err: "Invalid custom url"})
-        var existing = await Url.findOne( { shorturl: req.body.custom })
-        if (existing) return res.json({sucess: false, err: "Custom url already taken"})
-        shorturl = req.body.custom
-        iscustom = true
-    } else {
-        shorturl = await generateURL()
-    }
-    const data = { url: req.body.url, shorturl, iscustom }
-    new Url(data).save().then(() => {res.json({sucess: true, data})}).catch(() => {res.json({sucess: false, err: "Error saving to database."})})
+  let shorturl = false
+  let iscustom = false
+  if (!req.body.url) return res.json({sucess: false, err: 'No url provided'})
+  if (!/http:\/\/|https:\/\//g.test(req.body.url)) return res.json({sucess: false, err: 'Invalid url'})
+  if (req.body.custom) {
+    if (/[^a-zA-Z0-9]/g.test(req.body.custom)) return res.json({sucess: false, err: 'Invalid custom url'})
+    let existing = await Url.findOne({shorturl: req.body.custom})
+    if (existing) return res.json({sucess: false, err: 'Custom url already taken'})
+    shorturl = req.body.custom
+    iscustom = true
+  } else {
+    shorturl = await generateURL()
+  }
+  const data = { url: req.body.url, shorturl, iscustom }
+  new Url(data).save().then(() => { res.json({sucess: true, data}) }).catch(() => { res.json({sucess: false, err: 'Error saving to database.'}) })
 })
 
-function generateURL() {
-    return new Promise(async (resolve, reject) => {
-        var shorturl = Math.random().toString(36).substring(7)
-        var existing = await Url.findOne( { shorturl })
-        resolve(existing ? await generateURL() : shorturl)
-    })
+function generateURL () {
+  return new Promise(async (resolve, reject) => {
+    let shorturl = Math.random().toString(36).substring(7)
+    let existing = await Url.findOne({ shorturl })
+    resolve(existing ? await generateURL() : shorturl)
+  })
 }
 
 app.post('/status', async (req, res) => {
-    if (req.body.list) {
-        var list = JSON.parse(req.body.list)
-        var result = await Promise.all(list.map(async item => {
-            var dbentry = await Url.findOne({ shorturl: item.shorturl })
-            item['clicks'] = dbentry ? dbentry.clicks : 0
-            return item
-        }))
-        res.json(result)
-    }
+  if (req.body.list) {
+    let list = JSON.parse(req.body.list)
+    let result = await Promise.all(list.map(async item => {
+      let dbentry = await Url.findOne({ shorturl: item.shorturl })
+      item['clicks'] = dbentry ? dbentry.clicks : 0
+      return item
+    }))
+    res.json(result)
+  }
 })
 
 app.get('/:shorturl', async (req, res) => {
-    var url = await Url.findOneAndUpdate( { shorturl: req.params.shorturl }, {$inc: { clicks:1 } } )
-    res.redirect(url ? url.url : '/')
+  let url = await Url.findOneAndUpdate({ shorturl: req.params.shorturl }, {$inc: {clicks: 1}})
+  res.redirect(url ? url.url : '/')
 })
 
 app.listen(config.port, () => { console.log(`Listening on port ${config.port}`) })
--- static/js/app.js
+++ static/js/app.js
@@ -2,48 +2,48 @@ if (!localStorage.getItem('history')) {
   localStorage.setItem('history', JSON.stringify([]))
 }
 
-function short() {
-  var url = $("#url").val()
-  var custom = $("#customurl").val()
+function short () {
+  let url = $('#url').val()
+  let custom = $('#customurl').val()
 
   if (url && /http:\/\/|https:\/\//g.test(url)) {
     if (/[^a-zA-Z0-9]/g.test(custom)) {
-      M.toast({html: 'Invalid custom url', classes: 'errtoast'});
-      return;
+      M.toast({html: 'Invalid custom url', classes: 'errtoast'})
+      return
     }
-    var postdata = { url, custom }
-    $.post("/short", postdata, (res) => {
+    let postdata = { url, custom }
+    $.post('/short', postdata, (res) => {
       if (res.sucess) {
-        var history = JSON.parse(localStorage.getItem('history'))
+        let history = JSON.parse(localStorage.getItem('history'))
         history.push(res.data)
         localStorage.setItem('history', JSON.stringify(history))
         updatehistory()
       } else {
-        M.toast({html: res.err, classes: 'errtoast'});
+        M.toast({html: res.err, classes: 'errtoast'})
       }
     }, 'json')
   } else {
-    M.toast({html: 'Invalid url', classes: 'errtoast'});
+    M.toast({html: 'Invalid url', classes: 'errtoast'})
   }
 }
 
-function updatehistory(updateviews) {
-  var history = JSON.parse(localStorage.getItem('history'))
+function updatehistory (updateviews) {
+  let history = JSON.parse(localStorage.getItem('history'))
   if (typeof history !== 'undefined' && history.length > 0) {
     if (updateviews) {
-      var preres = JSON.parse(localStorage.getItem('history'))
+      let preres = JSON.parse(localStorage.getItem('history'))
       preres.reverse()
-      var preentries = preres.map(url => {return mapentries(url, true)})
+      let preentries = preres.map(url => {return mapentries(url, true)})
       renderTable(preentries)
-      $.post("/status", {list: localStorage.getItem('history')}, (res) => {
+      $.post('/status', {list: localStorage.getItem('history')}, (res) => {
         localStorage.setItem('history', JSON.stringify(res))
         res.reverse()
-        var entries = res.map(url => {return mapentries(url, false)})
+        let entries = res.map(url => { return mapentries(url, false) })
         renderTable(entries)
       }, 'json')
     } else {
       history.reverse()
-      var entries = history.map(url => {return mapentries(url, false)})
+      let entries = history.map(url => { return mapentries(url, false) })
       renderTable(entries)
     }
   }
@@ -51,22 +51,23 @@ function updatehistory(updateviews) {
 
 $(document).ready(updatehistory)
 
-function mapentries(url, loading) {
-  return `<tr><td><p class="shorturl" data-clipboard-text="${window.location.href + url.shorturl}">${window.location.href + url.shorturl}</p></td><td><a href="${url.url}">${url.url}</a></td><td>${loading ? '<div class="preloader-wrapper active" style="width: 25px;height: 25px;"><div class="spinner-layer spinner-blue-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>' : `${url.clicks||0} clicks`}</td></tr>`
+function mapentries (url, loading) {
+  return `<tr><td><p class="shorturl" data-clipboard-text="${window.location.href + url.shorturl}">${window.location.href + url.shorturl}</p></td><td><a href="${url.url}">${url.url}</a></td><td>${loading ? '<div class="preloader-wrapper active" style="width: 25px;height: 25px;"><div class="spinner-layer spinner-blue-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>' : `${url.clicks || 0} clicks`}</td></tr>`
 }
 
-function renderTable(entries) {
+function renderTable (entries) {
   $('#table').html(`<table class="highlight"><thead><tr><th data-field="url">Short Url</th><th data-field="short">Long Url</th></tr></thead><tbody>${entries.join('')}</tbody></table>`)
-  $(".shorturl").unbind("click");
-  $(".shorturl").click(copy)
-  new ClipboardJS('.shorturl')
+  $('.shorturl').unbind('click')
+  $('.shorturl').click(copy)
+  let clipboard = new ClipboardJS('.shorturl')
+  clipboard.on('success', e => { e.clearSelection() })
 }
 
-function copy() {
-  var width = $(this).width()
-  var link = $(this).html()
-  if (link == 'Copied.') {
-    return;
+function copy () {
+  let width = $(this).width()
+  let link = $(this).html()
+  if (link === 'Copied.') {
+    return
   }
   $(this).html('Copied.')
   $(this).width(width)
